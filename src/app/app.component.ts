082326f26de22ccde4ebd5f97215146582daff86
DavidG2115 <l20120108@morelia.tecnm.mx>
2024-05-12 01:04:20 -0600

    Modificaciones del nav para juez y cerrar sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
+  constructor(private router: Router) {}
   title = 'syspol';
+  
   private logUserData() {
     const usuario = localStorage.getItem('usuario');  // Asume que guardas el nombre de usuario como 'usuario'
     const role = localStorage.getItem('role');        // Asume que el rol está guardado como 'role'
 
     console.log('Datos del usuario:', { usuario, role });
   }
+  
   ngOnInit() {
     this.logUserData();
+    initFlowbite();
+  }
+  isJuezModule(): boolean {
+    // Verifica si la ruta actual pertenece al módulo "juez"
+    return this.router.url.includes('/juez');
+    
+  }
+  isCarDetailModule(): boolean {
+    return this.router.url.startsWith('/car-detail');
+  }
+
+  logout() {
+    localStorage.removeItem('token');  // Eliminar el token de autenticación
+    this.router.navigate(['/login']);  // Redirigir al usuario al login
   }
 
 }
